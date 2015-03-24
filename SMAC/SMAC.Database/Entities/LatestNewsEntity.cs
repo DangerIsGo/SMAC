f1c15f8c821f04faccafd63ff9b15fc433ac3bca
Devon <DangerIsGo@gmail.com>
2015-03-24 02:23:42 -0400

    Fixed include for latest news

--- SMAC/SMAC.Database/Entities/LatestNewsEntity.cs
+++ SMAC/SMAC.Database/Entities/LatestNewsEntity.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data.Entity;
 
 namespace SMAC.Database
 {
@@ -15,9 +16,9 @@ namespace SMAC.Database
                 {
 
                     if (count.HasValue)
-                        return (from a in context.LatestNewsSet where a.SchoolId == schoolId select a).OrderByDescending(a => a.PostedAt).ToList().Take(count.Value).ToList();
+                        return (from a in context.LatestNewsSet where a.SchoolId == schoolId select a).Include(t=>t.User).OrderByDescending(a => a.PostedAt).ToList().Take(count.Value).ToList();
                     else
-                        return (from a in context.LatestNewsSet where a.SchoolId == schoolId select a).OrderByDescending(a => a.PostedAt).ToList().ToList();
+                        return (from a in context.LatestNewsSet where a.SchoolId == schoolId select a).Include(t => t.User).OrderByDescending(a => a.PostedAt).ToList().ToList();
                 }
             }
             catch (Exception ex)
